aceaba3e6e0f39bb014515c849fe380db23757c9
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-13 20:42:40 -0500

    Update sky.py

--- src/sky.py
+++ src/sky.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import tkk
+from tkinter import ttk
 import pymem as pm
 import os
 import time
@@ -7,18 +7,21 @@ import time
 processes = []
 
 def getprocess():
-    for process
+    for process in #runningprocesses:
+        processes.append(process)
 
 def app():
+    global processes
     sky = tk.Tk()
     sky.geometry("280x320")
     sky.iconbitmap("logo.ico")
     processlabel = tk.Label(sky, text="Welcome to Sky\nSelect Process:")
     process_option = tk.StringVar()
     process_option.set("Select Process")
-    
+    process_selected = ttk.OptionMenu(sky, process_option, *processes)
     
     processlabel.pack(pady=10)
+    process_selected.pack(pady=10)
     sky.mainloop()
 
 if __name__ == "__main__":
