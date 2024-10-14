f1dafba50ee4e6c8a7b1e79c83c84fdbc8d430d3
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-13 20:31:28 -0500

    Update sky.py

--- src/sky.py
+++ src/sky.py
@@ -1,10 +1,13 @@
 import tkinter as tk
 import pymem as pm
+import os
+import time
 
 def app():
     sky = tk.Tk()
     sky.geometry("280x320")
     sky.iconbitmap("logo.ico")
+    processlabel = Label(sky
     sky.mainloop()
 
 if __name__ == "__main__":
