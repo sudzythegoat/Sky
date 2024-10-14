b3639661ead91fc09be4a973ff7c5f11d445cbe6
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-13 20:33:47 -0500

    Update sky.py

--- src/sky.py
+++ src/sky.py
@@ -7,7 +7,9 @@ def app():
     sky = tk.Tk()
     sky.geometry("280x320")
     sky.iconbitmap("logo.ico")
-    processlabel = Label(sky
+    processlabel = tk.Label(sky, text="Welcome to Sky\nSelect Process:")
+    
+    processlabel.pack(pady=10)
     sky.mainloop()
 
 if __name__ == "__main__":
