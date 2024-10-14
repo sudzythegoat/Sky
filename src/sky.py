36e55c6dfa202113c1b3b122680005a2ee765c54
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-13 20:30:11 -0500

    Update sky.py

--- src/sky.py
+++ src/sky.py
@@ -4,4 +4,8 @@ import pymem as pm
 def app():
     sky = tk.Tk()
     sky.geometry("280x320")
-    sky.mainloop
+    sky.iconbitmap("logo.ico")
+    sky.mainloop()
+
+if __name__ == "__main__":
+    app():
