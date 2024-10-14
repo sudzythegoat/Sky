4deb2dbb7ce33ac4ebddf3745494fa821a80303e
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-14 07:33:56 -0500

    Update sky.py

--- src/sky.py
+++ src/sky.py
@@ -16,6 +16,11 @@ def getprocess():
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
 
+def savead():
+    try:
+        with open("saved.txt", "r") as file:
+            saved_stuff = file.read()
+
 def app():
     sky = tk.Tk()
     sky.geometry("280x320")
@@ -24,11 +29,15 @@ def app():
     process_option = tk.StringVar()
     process_option.set("Select Process")
     process_selected = ttk.OptionMenu(sky, process_option, *processes)
-    
-    
+    adress_label = tk.Label(sky, text="Memory Adress:")
+    adress_input = tk.Entry(sky)
+    save_adress = tk.Button(sky, text="Save Adress", command: savead)
     
     processlabel.pack(pady=10)
     process_selected.pack(pady=10)
+    adress_label.pack(pady=10)
+    adress_input.pack(pady=10)
+    save_adress.pack(pady=10)
     sky.mainloop()
 
 if __name__ == "__main__":
